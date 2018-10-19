b362d9899e849ab610ab7e63a226030758d1196b
Marco <mcg.tampieri@gmail.com>
2018-10-19 10:45:13 +0200

    Training 5 done

--- questions.js
+++ questions.js
@@ -10,55 +10,58 @@ var remplaceECar = function (texte) {
     return texte.replace("e", " ");
 }
 var concatString = function (texte1, texte2) {
-
+    return texte1.concat(texte2);
 }
 var afficherCar5 = function (texte) {
-
+    return texte.charAt(4);
 }
 var afficher9Car = function (texte) {
-
+    return texte.slice(0, 9);
 }
 var majusculeString = function (texte) {
-
+    return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
-
+    return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-
+    return texte.trim();
 }
 var IsString = function (texte) {
-
+    return texte.includes(texte);
 }
 
 var AfficherExtensionString = function (texte) {
-
+    return texte.split(".").pop();
 }
 var NombreEspaceString = function (texte) {
-
+    return texte.split(" ").length - 1;
 }
 var InverseString = function (texte) {
-
+    let a = texte.split("");
+    let b = a.reverse();
+    let c = b.join("");
+    return c;
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+    return Math.pow(x, y);
 }
 var valeurAbsolue = function (nombre) {
-
+    return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-
+    return array.map(x => Math.abs(x));
 }
 var sufaceCercle = function (rayon) {
-
+    return Math.round(Math.pow(rayon, 2) * Math.PI);
 }
 var hypothenuse = function (ab, ac) {
-
+    return Math.hypot(ab, ac);
 }
 var calculIMC = function (poids, taille) {
-
+    return Math.round(poids / Math.pow(taille, 2) *100) / 100;
 }
